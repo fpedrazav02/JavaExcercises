e6f02b9d348992bcfd3b29db08e3b6bb8b26a798
franpedrazav <fpedraza@typsa.es>
2024-03-01 12:31:50 +0100

    Adding MOD1 Exercise 2

--- MOD1/M1_02.java
+++ MOD1/M1_02.java
@@ -1,13 +1,14 @@
 /*
+
 Crear un programa que realice algunas operaciones matemáticas.
 1. Pedir por consola al usuario que introduzca 3 números enteros y almacenar su valor en variables.
 2. Operación 1 : a*b/c Mostrar por consola el resultado
 3. Operación 2 : (a*c)%b Mostrar por consola el resultado
 4. Operación 3 : 2*(a+c-b)/(b*c) Mostrar por consola el resultado
 5. Operación 4 : ((a*c)+(b%a))/a-c Mostrar por consola el resultado
-
 6. Opcional: Repetir las operaciones con números decimales para ver las diferencias de resultado. 
- */
+
+*/
 
 import java.util.Scanner;
 
@@ -17,8 +18,10 @@ public class M1_02 {
         
         // A
         System.out.println("\nIntroduce el primer número:");
+
         double a = scanner.nextDouble();
         int decimalA = (int)a;
+
         
         // B
         System.out.println("\nIntroduce el segundo número:");
@@ -35,6 +38,22 @@ public class M1_02 {
 
         System.out.println(a + "," + b + "," + c);
 
+        //Operaciones
+        // I a*b/c
+        System.out.println("\nResultado de la operación I con decimales [(a*b)/c)]: " + ((a*b)/c));
+        System.out.println("\nResultado de la operación I sin decimales [(a*b)/c)]: " + ((decimalA*decimalB)/decimalC));
+       
+        // II (a*c)%b
+        System.out.println("\nResultado de la operación II con decimales [(a*c)%b]: " + ((a*c)%b));
+        System.out.println("\nResultado de la operación II sin decimales [(a*c)%b]: " + ((decimalA*decimalB)/decimalC));
+
+        // III 2*(a+c-b)/(b*c)
+        System.out.println("\nResultado de la operación III con decimales [2*(a+c-b)/(b*c)]: " + (2*(a+c-b)/(b*c)));
+        System.out.println("\nResultado de la operación III sin decimales [2*(a+c-b)/(b*c)]: " + ((decimalA*decimalB)/decimalC));
+   
+        // IV ((a*c)+(b%a))/a-c
+        System.out.println("\nResultado de la operación IV con decimales [((a*c)+(b%a))/a-c]: " + (((a*c)+(b%a))/a-c));
+        System.out.println("\nResultado de la operación IV sin decimales [((a*c)+(b%a))/a-c]: " + ((decimalA*decimalB)/decimalC));
     }
     
 }
